ac1df5aea7003e98040753a6dfe51b9642a2b116
Hema <Hema@DESKTOP-R4551PM>
2025-07-24 19:00:00 +0530

    Assignment 3

--- C2TC_JavaProject_Assignment/src/com/hema/assignment3/BankDemo.java
+++ C2TC_JavaProject_Assignment/src/com/hema/assignment3/BankDemo.java
@@ -8,11 +8,11 @@ public class BankDemo {
 
         System.out.println("Total Accounts: " + Bank.getTotalAccounts());
 
-        Account savings = new SavingsAccount("Hem", 5000);
-        Account checking = new CheckingAccount("Abi", 3000);
+        Account savings = new SavingsAccount("Grace", 5000);
+        Account checking = new CheckingAccount("Geo", 3000);
 
-        Account savings1 = new SavingsAccount("Deepi", 5000);
-        Account checking1 = new CheckingAccount("Harshu", 3000);
+        Account savings1 = new SavingsAccount("harini", 5000);
+        Account checking1 = new CheckingAccount("jeni", 3000);
 
         Transaction txn = new Transaction();
         txn.performTransaction(savings, "deposit", 1000);
